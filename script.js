706b25d9e92e0c25d663a104e589d3809af0a117
graphixitystudio <graphixitystudio@gmail.com>
2025-12-19 00:15:06 +0500

    Add script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const reveals = document.querySelectorAll(".reveal");
+const items = document.querySelectorAll(".reveal");
 
-function animateOnScroll() {
-  reveals.forEach((el, i) => {
+function revealOnScroll(){
+  items.forEach((el,i)=>{
     const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
-      setTimeout(() => {
-        el.classList.add("active");
-      }, i * 120);
+    if(top < window.innerHeight - 100){
+      setTimeout(()=> el.classList.add("active"), i*120);
     }
   });
 }
 
-window.addEventListener("scroll", animateOnScroll);
-animateOnScroll();
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
